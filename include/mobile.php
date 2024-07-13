0d493e84a2ddf79f55a06e955b5acfc58d0a1723
ImAadarsh <aadarshkavita@gmail.com>
2024-07-14 03:59:14 +0530

    Final Pre Test

--- include/mobile.php
+++ include/mobile.php
@@ -6,50 +6,20 @@
             </div>
             <div class="th-mobile-menu">
                 <ul>
-                    <li class="menu-item-has-children">
+                     <li>
                         <a href="index.php">Home</a>
-                        <ul class="sub-menu">
-                            <li><a href="index.php">Restaurant Shop</a></li>
-                            <li><a href="home-fast-food.php">Home Fast Food</a></li>
-                            <li><a href="home-restaurant.php">Home Restaurant</a></li>
-                        </ul>
                     </li>
-                    <li class="menu-item-has-children">
-                        <a href="#">Pages</a>
-                        <ul class="sub-menu">
-                            <li><a href="about.php">About Us</a></li>
-                            <li><a href="service.php">Service</a></li>
-                            <li><a href="service-details.php">Service Details</a></li>
-                            <li><a href="team.php">Chef</a></li>
-                            <li><a href="team-details.php">Chef Details</a></li>
-                            <li><a href="reservation.php">Reservation</a></li>
-                            <li><a href="gallery.php">Gallery</a></li>
-                            <li><a href="error.php">Error Page</a></li>
-                        </ul>
+                    <li>
+                        <a href="menu.php">Menu</a>
                     </li>
-                    <li class="menu-item-has-children">
-                        <a href="#">Menus</a>
-                        <ul class="sub-menu">
-                            <li><a href="menu-one.php">Food Menu 1</a></li>
-                            <li><a href="menu-two.php">Food Menu 2</a></li>
-                        </ul>
+                    <li>
+                        <a href="about.php">About</a>
                     </li>
-                    <li class="menu-item-has-children">
-                        <a href="#">Shop</a>
-                        <ul class="sub-menu">
-                            <li><a href="shop.php">Shop</a></li>
-                            <li><a href="shop-details.php">Shop Details</a></li>
-                            <li><a href="cart.php">Cart Page</a></li>
-                            <li><a href="checkout.php">Checkout</a></li>
-                            <li><a href="wishlist.php">Wishlist</a></li>
-                        </ul>
+                    <li>
+                        <a href="service.php">Service</a>
                     </li>
-                    <li class="menu-item-has-children">
-                        <a href="#">Blog</a>
-                        <ul class="sub-menu">
-                            <li><a href="blog.php">Blog</a></li>
-                            <li><a href="blog-details.php">Blog Details</a></li>
-                        </ul>
+                    <li>
+                        <a href="gallery.php">Gallery</a>
                     </li>
                     <li>
                         <a href="contact.php">Contact</a>
